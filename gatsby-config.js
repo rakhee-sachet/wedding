bd93e3d86ae228218e5f61bc38c39473907db9a8
Rakhee Singh <rakheesingh@Rakhees-MBP.cable.rcn.com>
2019-11-09 19:15:09 -0500

    added configuration for file system and images

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,44 @@
 module.exports = {
   siteMetadata: {
-    title: 'Dana Cohen & Christopher Bell – October 20th, 2018 – Bayonet Farm, NJ',
-  },
+		title: `Rakhee & Sachet – Nov 06th, 2019 – Manhattan, NY`,
+		author: `Rakhee & Sachet`,
+		description: `Wedding Website`,
+	},
   plugins: [
     'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#663399',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+        icon: 'src/images/background.jpg' // This path is relative to the root of the site.
+      }
+    },
     'gatsby-plugin-styled-components',
+    {
+			resolve: `gatsby-plugin-material-ui`,
+    },
+    {
+			resolve: `gatsby-plugin-google-fonts`,
+			options: {
+			  fonts: [
+				`material icons`,
+			  ],
+			}
+    },
     // {
     //   resolve: 'gatsby-plugin-google-analytics',
     //   options: {
