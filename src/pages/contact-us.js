3e5f02f832afbcba9ea0d6790aee91342d40ca4a
Rakhee Singh <36114594+Rakhee06@users.noreply.github.com>
2020-02-16 22:07:56 -0500

    removed contacts

--- src/pages/contact-us.js
+++ src/pages/contact-us.js
@@ -18,9 +18,7 @@ const MiscPage = () => (
     <SectionTitle>Contacting Us</SectionTitle>
     <SectionBody>
       <ul>
-        <li>Rakhee: +1 646 400 8600 (also available on whatsapp)</li>
         <li>Email: rakheesingh.06@gmail.com</li>
-        <li>Sachet: +1 646 961 0056 (also available on whatsapp)</li>
         <li>Email: sachetpatil89@gmail.com</li>
         <li>Address: 601 E 20th St, Apt 5E, NY, NY, 10010</li>
       </ul>
